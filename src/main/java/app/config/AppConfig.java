83846a98f31e639fbe21629e012263a9d6953b50
PlatonOff7187 <sergey.platonov.87@mail.ru>
2021-07-06 23:34:20 +0300

    commit213

---
+++ src/main/java/app/config/AppConfig.java
@@ -0,0 +1,22 @@
+package app.config;
+
+import app.model.AnimalsCage;
+import app.model.Timer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "app")
+public class AppConfig {
+    @Bean(name = "animalsCage")
+    public AnimalsCage getAnimalsCage() {
+        AnimalsCage animalsCage = new AnimalsCage();
+        return animalsCage;
+    }
+    @Bean(name = "timer")
+    public Timer getTimer(){
+        Timer timer = new Timer();
+        return timer;
+    }
+}
---
+++ src/test/java/config/AppConfigTest.java
@@ -0,0 +1,7 @@
+package config;
+
+import junit.framework.TestCase;
+
+public class AppConfigTest extends TestCase {
+
+}
